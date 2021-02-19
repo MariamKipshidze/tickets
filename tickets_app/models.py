52c1c49dd02c2b27705dd4331147f3d5ddec8e48
mariami <mari.kifshidze@gmail.com>
2021-02-19 22:37:21 +0400

    adding ticket and order models

--- tickets_app/models.py
+++ tickets_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from users.models import User
 
-# Create your models here.
+
+class Ticket(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_("Name")),
+    price = models.DecimalField(max_digits=4, decimal_places=2, verbose_name=_('Price'))
+    start_date = models.DateTimeField(verbose_name=_("Start Date"))
+    end_date = models.DateTimeField(verbose_name=_("End date"))
+    barcode = models.PositiveSmallIntegerField(verbose_name=_("Barcode"), unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Ticket')
+        verbose_name_plural = _('tickets')
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.CASCADE, related_name="order")
+    ticket = models.OneToOneField(Ticket, verbose_name=("Ticket"), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ticket
+
+    class Meta:
+        verbose_name = _('Order')
+        verbose_name_plural = _('Orders')
